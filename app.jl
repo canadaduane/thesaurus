f79c5fbbddec07d9baf70053860972e00d7e5c0f
Duane Johnson <duane.johnson@gmail.com>
2014-12-30 21:58:19 -0700

    Update server

--- app.jl
+++ app.jl
@@ -1,8 +1,9 @@
 using Morsel
+import JSON
 
 include("WordMatrix.jl")
 
-m = WordMatrix("crawl300-100k")
+m = WordMatrix("/Users/duane/tmp/thesaurus/crawl300-100k")
 
 hotness = vectorSumOfWords(m, [
   "hot", "warm", "sun", "sunshine", "rays", "shining", "light", "burn", 
@@ -19,14 +20,60 @@ hotcold = WordMatrix({"hot" => hotness, "cold" => coldness})
 
 app = Morsel.app()
 
-get(app, "/hot-or-cold/<word::String>") do req, res
-    word = req.params[:word]
-    ranked = nearest(hotcold, m[word])
-    ranked[1][1]
+function jsonResponse(res, data)
+  res.headers["Content-Type"] = "application/json"
+  JSON.json(data)
+end
+
+get(app, "/hot-cold/<word::String>") do req, res
+  word = req.params[:word]
+  ranked = nearest(hotcold, m[word])
+  println("hot-cold: ", word, " ", ranked[1][1])
+  ranked[1][1]
+end
+
+get(app, "/lookup") do req, res
+  if haskey(req.state, :url_params)
+    params = req.state[:url_params]
+    n = int(get(params, "n", "30"))
+    if haskey(params, "words")
+      spacedWords = replace(get(params, "words", ""), r"[:;., ]+", " ")
+      words = convert(Array{ASCIIString}, split(spacedWords, ' '))
+      println("lookup: ", words)
+      try
+        quality = vectorSumOfWords(m, words)
+        ranked = topn(m, quality, n)
+        jsonResponse(res, ranked)
+      catch e
+        if isa(e, WordNotFound)
+          return jsonResponse(res, {"error" => "'$(e.word)' not in dataset"})
+        else
+          return jsonResponse(res, {"error" => "unable to find vector sum of words"})
+        end
+      end
+    else
+      jsonResponse(res, {"error" => "'words' is a required param"})
+    end
+  else
+    jsonResponse(res, {"error" => "params required"})
+  end
 end
 
 get(app, "/") do req, res
-    "Duane's Thesaurus"
+"""
+<html>
+<head><title>GloVe Thesaurus</title></head>
+<body>
+  <h2>Duane's <a href="http://nlp.stanford.edu/projects/glove/">GloVe</a> Thesaurus</h2>
+
+  <form method="GET" action="/lookup">
+    <div><label for="words">Words (separated by spaces or commas):</label></div>
+    <input type="text" name="words" value="" />
+    <input type="submit" value="Lookup" />
+  </form>
+</body>
+</html>
+"""
 end
 
 start(app, 8000)
